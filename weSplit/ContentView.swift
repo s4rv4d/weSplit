560ff722bbc87ad29cf6e051fb9b2fe143c2c30c
s4rv4d <sarvadshetty@gmail.com>
2019-11-14 07:14:53 +0530

    Initial Commit

---
+++ weSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weSplit
+//
+//  Created by Sarvad shetty on 11/14/19.
+//  Copyright © 2019 Sarvad shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
