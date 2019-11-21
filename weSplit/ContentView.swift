1214e0be050d813e180acf8141623b59a74b15ea
s4rv4d <sarvadshetty@gmail.com>
2019-11-21 21:49:52 +0530

    added comments and practice stuff

--- weSplit/ContentView.swift
+++ weSplit/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    let studentName = ["Harry","server","connected"]
+    @State private var studName = "Harry"
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        Picker("Selected Student", selection: $studName) {
+            ForEach(0..<studentName.count){
+                Text("\(self.studentName[$0])")
+            }
+        }
+        
+//        Form {
+//            ForEach(0 ..< 100){ number in
+//                Text("Row \(number)")
+//            }
+//        }
+        
+////        Form {
+//////            This is what’s called a two-way binding: we bind the text field so that it shows the value of our property, but we also bind it so that any changes to the text field also update the property.
+////            TextField("Enter name:", text: $name)
+////            Text("Your name is \(name)")
+////        }
+        
+        ////some basically means it should return the same kind of view
+        ////can only add upto 10 elements in a block in swiftUI
+////        Button("Tap count \(tapCount)") {
+////            self.tapCount += 1
+////        }
+////        NavigationView {
+////            Form {
+////                Section {
+////                    Text("Hello, World!")
+////                }
+////            }
+////
+////            //setting a modifier(basically a property)
+////            .navigationBarTitle(Text("SwiftUI")) or
+////                .navigationBarTitle(Text("SwiftUI"), displayMode: .inline)
+////                .navigationBarTitle("SwiftUI", displayMode: .inline)
+////        }
     }
 }
 
